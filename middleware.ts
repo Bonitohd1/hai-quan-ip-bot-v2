b9da9fcbbf85a6da9a3bb4a0e31bde8f53e7e700
Your Name <you@example.com>
2026-04-03 03:10:43 +0700

    fix(vercel): Explicit middleware export for Next.js Edge compiler AST

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth;
 
 export const config = {
   // Bắt buộc đăng nhập cho toàn bộ ứng dụng, ngoại trừ api, _next/static, _next/image, favicon.ico và auth
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth).*)'],
 };
+
